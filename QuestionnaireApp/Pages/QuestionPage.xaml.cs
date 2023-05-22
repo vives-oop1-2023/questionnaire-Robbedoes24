f666c5e3c093e4e9cd3aea885352d2e4fb01d04d
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-05-22 19:05:23 +0200

    added styling to other pages

--- QuestionnaireApp/Pages/QuestionPage.xaml.cs
+++ QuestionnaireApp/Pages/QuestionPage.xaml.cs
@@ -48,25 +48,61 @@ namespace QuestionnaireApp.Pages
                 button.Content = answer.Text;
                 button.Tag = answer;
                 button.Click += new RoutedEventHandler(OnClickAnswer);
+                button.Style = FindResource("AnswerButton") as Style;
 
                 AnswerList.Children.Add(button);
             }
         }
 
-        void OnClickAnswer(object sender, RoutedEventArgs e)
+        async void OnClickAnswer(object sender, RoutedEventArgs e)
         {
-            Button? button = sender as Button;
+            Button? clickedButton = sender as Button;
             // TODO: Return exeption if button is null
 
-            Answer? answer = button.Tag as Answer;
+            Answer? clickedAnswer = clickedButton.Tag as Answer;
             // TODO: Return exeption if answer is null
 
+            //Disable all buttons (no extra clicks)
+            Button? correctButton = new Button();
+            Answer? correctAnswer = new Answer("",false);
+            for (int i = 0; i < AnswerList.Children.Count; i++)
+            {
+                Button button = AnswerList.Children[i] as Button;
+                button.IsEnabled = false;
+                Answer answer = button.Tag as Answer;
+                if (answer.IsCorrect)
+                {
+                    correctButton = button;
+                    correctAnswer = answer;
+                }
+            }
+
+            // Change button background if correct or incorrect and wait a few seconds
+            if (clickedAnswer.IsCorrect)
+            {
+                clickedButton.Background = FindResource("Button.Answer.Right") as SolidColorBrush;
+
+                // wait x seconds to the user can see their answer was correct;
+                await Task.Delay(baseWaitTime);
+            }
+            else
+            {
+                clickedButton.Background = FindResource("Button.Answer.Wrong") as SolidColorBrush;
+                correctButton.Background = FindResource("Button.Answer.Right") as SolidColorBrush;
+
+                // wait x seconds to the user can see and read the correct answer;
+                await Task.Delay(baseWaitTime + (millisecondsPerLetter * correctAnswer.Text.Length));
+            }
+
             // Call question answered event
-            QuestionAnswered(this, new QuestionAnsweredEventArgs(answer));
+            QuestionAnswered(this, new QuestionAnsweredEventArgs(clickedAnswer));
         }
 
         // Create eventhandler that will be called when question is answered
         public event EventHandler<QuestionAnsweredEventArgs> QuestionAnswered;
+
+        private readonly int baseWaitTime = 500;
+        private readonly int millisecondsPerLetter = 200;
     }
 
     public class QuestionAnsweredEventArgs : EventArgs
