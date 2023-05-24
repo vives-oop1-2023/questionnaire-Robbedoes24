e3efc4f5ceddd51d94a11db053c8d7f7faaa2931
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-05-24 15:53:00 +0200

    removed unused modules

--- QuestionnaireApp/MainWindow.xaml.cs
+++ QuestionnaireApp/MainWindow.xaml.cs
@@ -1,27 +1,10 @@
 ﻿using GameLibrary;
 using QuestionnaireApp.Pages;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Xml.Linq;
 using TriviaApiLibrary;
 
 namespace QuestionnaireApp
